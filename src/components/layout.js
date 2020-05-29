7fea9346c829f96c5320a9d3cb17393f7cd7e167
sggv950 <38014323+sggv950@users.noreply.github.com>
2020-05-29 23:25:50 +0300

    fixed layout file + clean cache

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,7 +35,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main
+        <main>{children}</main>
       </div>
     </FirebaseContext.Provider>
   )
